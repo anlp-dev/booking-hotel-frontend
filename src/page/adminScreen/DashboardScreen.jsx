b22a38530d5a1ef9d626d87f09e39fda23428b6a
kimquy <kimquy001623@gmail.com>
2025-03-09 18:18:52 +0700

    create UI of room for admin

--- src/page/adminScreen/DashboardScreen.jsx
+++ src/page/adminScreen/DashboardScreen.jsx
@@ -1,43 +1,48 @@
-import * as React from 'react';
-
-import Box from '@mui/material/Box';
-import SideMenu from '../../components/adminScreen/SideMenu';
+import Box from "@mui/material/Box";
+import SideMenu from "../../components/adminScreen/SideMenu";
 import HomeAdmin from "./HomeAdmin.jsx";
-import {Route, Routes, useNavigate} from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import LogRequest from "./LogRequest.jsx";
 import ManageUser from "./UserManagement.jsx";
 import RoleBaseRoute from "../../security/RoleBaseRoute.jsx";
 import RolePermissionManagement from "./RoleManage.jsx";
 import BookingManagement from "./BookingManagement.jsx";
-
+import RoomManagement from "./RoomManage.jsx";
 
 export default function Dashboard() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleMenuItemClick = (path) => {
-        navigate(`/admin/${path}`);
-    };
+  const handleMenuItemClick = (path) => {
+    navigate(`/admin/${path}`);
+  };
 
-    return (
-            <Box sx={{ display: 'flex' }}>
-                <SideMenu onMenuItemClick={handleMenuItemClick}/>
-                <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                    <Routes>
-                        <Route path="/homeAdmin" element={<HomeAdmin />} />
-                        <Route path="/logRequest" element={
-                            <RoleBaseRoute allowedRoles={["SUPER_ADMIN"]}>
-                                <LogRequest/>
-                            </RoleBaseRoute>
-                        }/>
-                        <Route path="/manageRole" element={
-                            <RoleBaseRoute allowedRoles={["SUPER_ADMIN"]}>
-                                <RolePermissionManagement/>
-                            </RoleBaseRoute>
-                        }/>
-                        <Route path="/manageUser" element={<ManageUser/>}/>
-                        <Route path="/bookingManagement" element={<BookingManagement/>}/>
-                    </Routes>
-                </Box>
-            </Box>
-    );
+  return (
+    <Box sx={{ display: "flex" }}>
+      <SideMenu onMenuItemClick={handleMenuItemClick} />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Routes>
+          <Route path="/homeAdmin" element={<HomeAdmin />} />
+          <Route
+            path="/logRequest"
+            element={
+              <RoleBaseRoute allowedRoles={["SUPER_ADMIN"]}>
+                <LogRequest />
+              </RoleBaseRoute>
+            }
+          />
+          <Route
+            path="/manageRole"
+            element={
+              <RoleBaseRoute allowedRoles={["SUPER_ADMIN"]}>
+                <RolePermissionManagement />
+              </RoleBaseRoute>
+            }
+          />
+          <Route path="/manageUser" element={<ManageUser />} />
+          <Route path="/bookingManagement" element={<BookingManagement />} />
+          <Route path="/manageRoom" element={<RoomManagement />} />
+        </Routes>
+      </Box>
+    </Box>
+  );
 }
