b5cebd0c92b65e7a47889e28becb9b4ee6e41c12
Trần Thanh Hải <96519556+haiThanhTran@users.noreply.github.com>
2025-03-10 01:02:38 +0700

    feat/register_facility_fe

--- src/components/adminScreen/MenuContent.jsx
+++ src/components/adminScreen/MenuContent.jsx
@@ -18,6 +18,7 @@ import BookOnlineIcon from '@mui/icons-material/BookOnline';
 import RouterIcon from '@mui/icons-material/Router';
 import {useLocation} from "react-router-dom";
 import BedroomChildIcon from "@mui/icons-material/BedroomChild";
+import BedIcon from "@mui/icons-material/Bed";
 
 const mainListItems = [
     {text: 'Trang chủ', icon: <HomeRoundedIcon/>, path: "homeAdmin"},
