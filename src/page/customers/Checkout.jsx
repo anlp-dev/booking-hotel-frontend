c1bd28587e3dcb58ae93b0a73c5c584a63aa51b6
anlp-dev <anlp.sep19@gmail.com>
2025-03-09 23:22:23 +0700

    update payment api

--- src/page/customers/Checkout.jsx
+++ src/page/customers/Checkout.jsx
@@ -41,6 +41,7 @@ import {
 } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
 import styles from '../../static/css/Checkout.module.css';
+import PaymentService from '../../services/PaymentService';
 
 const { Title, Text, Paragraph } = Typography;
 const { Header, Content } = Layout;
@@ -69,33 +70,33 @@ const Checkout = () => {
     grandTotal: 4140000
   };
 
-  const onFinish = (values) => {
-    setLoading(true);
-    console.log('Form values:', values);
-    
-    // Simulate API call
-    setTimeout(() => {
-      setLoading(false);
-      message.success('Đặt phòng thành công!');
-      
-      // Create booking data to pass to success page
-      const bookingData = {
-        bookingId: 'BK-' + Math.floor(100000 + Math.random() * 900000),
-        customerName: values.fullName || 'Khách hàng',
-        email: values.email || 'example@email.com',
-        phone: values.phone || '0912345678',
-        roomType: bookingDetails.roomType,
-        checkIn: bookingDetails.checkIn,
-        checkOut: bookingDetails.checkOut,
-        guests: bookingDetails.guests,
-        totalAmount: bookingDetails.grandTotal,
-        paymentMethod: values.paymentMethod || 'Credit Card',
-        paymentId: 'PAY-' + Math.floor(100000 + Math.random() * 900000)
+  const onFinish = async (values) => {
+    try{
+      setLoading(true);
+      console.log('Form values:', values);
+       // Create booking data to pass to success page
+       const bookingData = {
+        orderId: 'BK-' + Math.floor(100000 + Math.random() * 900000),
+        totalPrice: bookingDetails.grandTotal,
+        orderInfo: ``,
+        orderType: `Đặt phòng khách sạn`
       };
+  
+      bookingData.orderInfo = `Thanh toán phòng ${bookingDetails.hotelName} với mã đặt phòng ${bookingData.orderId}`;
+      bookingData.bankCode = 'NCB';
       
-      // Navigate to success page with booking data
-      navigate('/booking-success', { state: { bookingData } });
-    }, 1500);
+      const resData = await PaymentService.getUrlVnPay(bookingData);
+
+      if(resData.status === 200){
+        window.location.href = resData.data;
+      }else{
+        message.error(resData.message);
+      }
+    }catch(e){
+      message.error(e.message);
+    }finally{
+      setLoading(false);
+    }
   };
 
   // Format currency
